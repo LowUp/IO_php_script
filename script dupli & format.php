bf14e7977ceae1e584967eb22b87fb8ccac0f8ba
LowUp <credo05@hotmail.fr>
2022-06-22 19:57:19 +0200

    Deletes wrong input format in cell

--- script dupli & format.php
+++ script dupli & format.php
@@ -6,29 +6,44 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Shuchkin\SimpleXLSX;
 
+$date = date("Y-m-d");
 $file = 'noduplicates.xlsx';
-$output = 'test.xlsx';
+$output = "test-$date.xlsx";
+$columnTargeted = 1;
 
 $reader = IOFactory::createReader('Xlsx');
 $spreadsheet = $reader->load($file);
+
+function useRegex($input) {
+    $regex = '/([a-zA-Z]+(\\.[a-zA-Z]+)+)@fr\\.[a-zA-Z]ran[a-zA-Z]avi[a-zA-Z]\\.com/i';
+    return preg_match($regex, $input);
+}
+
+// $input1 = "MNI helene.orsini@fr.transavia.com";
+
+// print_r(useRegex($input1));
 
 if( $xlsx = SimpleXLSX::parse($file) ){
 
     $emailsContainer = [];
     print_r("loading...");
 
-    for($x = 0; $x < count( $xlsx->rows()); $x+=1)
+    for($x = 1; $x < count( $xlsx->rows()); $x+=1)
     {
         $sheet = $spreadsheet->getActiveSheet();
-        $email = $xlsx->rows()[$x][1];
-        $incrementor = 1;
-        // print_r($email);
+        $email = $xlsx->rows()[$x][$columnTargeted];
 
         if(!in_array($email, $emailsContainer)){
             $tempArray = array_push($emailsContainer, $email);
+            if(useRegex($email) == 0)
+            {
+                print_r("\nWrong input found at line " . $x+1);
+                $sheet->removeRow($x+1, $columnTargeted); // delete row
+                $sheet->insertNewRowBefore($x+1); // insert blank row where the deleted row was originally placed
+            }
         } else {
             print_r("\nDuplicate found at line " . $x+1);
-            $sheet->removeRow($x+1, 1); // delete row
+            $sheet->removeRow($x+1, $columnTargeted); // delete row
             $sheet->insertNewRowBefore($x+1); // insert blank row where the deleted row was originally placed
         }
     }
